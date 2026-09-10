5650fe0daf5acb0af7d4dc45df5ed7bbb442a98c
Joao Paulo Da Silva De Oliveira <joaosilva12oliveira@gmail.com>
2026-09-10 08:28:35 -0300

    Update script.js

--- projeto/script.js
+++ projeto/script.js
@@ -1,8 +1,10 @@
 console.log("Sistema iniciado");
+console.log("Bem-vindo ao sistema"); // + NOVA LINHA
 
 const botao = document.querySelector("#botao");
 const mensagem = document.querySelector("#mensagem");
 
 botao.addEventListener("click", () => {
-  mensagem.textContent = "Ação executada";
+  mensagem.textContent = "Ação executada com sucesso!"; // ~ linha alterada
+  console.log("Ação executada"); // + NOVA LINHA
 });
